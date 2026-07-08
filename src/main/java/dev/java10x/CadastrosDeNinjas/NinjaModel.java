ef252c520d12c7e5fccce6744ecc4475e0720695
jacksonx658-afk <seu_email_do_github@example.com>
2026-07-08 00:10:02 -0300

    Cria uma entidade no banco de dados a partir do NinjaMode[aula3]

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/NinjaModel.java
@@ -0,0 +1,48 @@
+package dev.java10x.CadastrosDeNinjas;
+
+import jakarta.persistence.*;
+
+// Entity trasnforma uma classe em uma entidade do Banco de Dados
+@Table(name = "tb_cadastro")
+@Entity
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
