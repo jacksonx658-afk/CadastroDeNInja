35e008242bccb1548c6daaa5566f9aa7514dbae8
jacksonx658-afk <seu_email_do_github@example.com>
2026-08-14 00:44:21 -0300

    Cria o repository e conecta ao JPA

--- src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesRepository.java
@@ -3,4 +3,5 @@ package dev.java10x.CadastrosDeNinjas.Missoes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissoesRepository extends JpaRepository< MissoesModel, Long> {
+
 }
