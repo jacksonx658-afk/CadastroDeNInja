3bfbedc4318020c21feb619ba6854c80d2701a9b
jacksonx658-afk <seu_email_do_github@example.com>
2026-08-25 16:23:55 -0300

    feat: Cria as rotas do controller com moc

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,38 @@
+package dev.java10x.CadastrosDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping
+@RestController("missoes")
+public class MissoesController {
+
+    // GET -- Mandar uma requisição para mostrar missões
+
+    @GetMapping("/mostrar")
+    public String ListarMissao(){
+        return "Missão listada com sucesso";
+    }
+
+    // POST -- Mandar uma requisição para criar as missões
+
+    @PostMapping("/criar")
+    public String CriaMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    // PUT -- Mandar uma requisição para alterar as missões
+
+    @PutMapping("/alterar")
+    public String AlterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    // DELETE -- Mandar uma requisição para deletar as missões
+
+    @DeleteMapping("/deletar")
+    public String DeletarMissoes(){
+        return "Missão deletada com sucesso";
+    }
+}
+
+
